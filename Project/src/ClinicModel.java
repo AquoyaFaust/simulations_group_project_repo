ef55ae84f474c111b1b854dc1ccd7e702fd60184
AquoyaFaust <48639292+AquoyaFaust@users.noreply.github.com>
2019-12-07 13:58:34 -0600

    Added in ClinicModel class

---
+++ Project/src/ClinicModel.java
@@ -0,0 +1,15 @@
+import desmoj.core.simulator.*;
+
+/**
+ * 
+ * @authors 
+ * Mai Chen
+ * Aquoya Faust
+ * Remington Steele
+ * 
+ * This Desmo-J based simulator will be a process oriented Model
+ * It simulates a 12 hour a day 8am-8pm clinic including Nurse Practioners and Specialists
+ */
+public class ClinicModel extends Model {
+
+}
