826766cfcb2f7cb73196918917f9e4f474a03c3f
AquoyaFaust <48639292+AquoyaFaust@users.noreply.github.com>
2019-12-07 14:30:52 -0600

    Commit

--- Project/src/Patient.java
+++ Project/src/Patient.java
@@ -14,9 +14,9 @@ public class Patient extends SimProcess{
 		ClinicModel model = (ClinicModel)getModel();
 		
 		model.numberInSystem.update();
-		if(model.nurseQueue >= model.queueThreshold) {
-			totalCost.update(500);
-			numberInSystem.update(-1);
+		if(model.nurseQueue.length() >= model.queueThreshold) {
+			model.totalCost.update(500);
+			model.numberInSystem.update(-1);
 			return;
 		} 
 		
